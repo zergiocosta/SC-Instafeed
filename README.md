6bd3d2e00f0c308f829731b74d668f19cd93f542
zergiocosta <sergio.costa@outlook.com>
2015-05-12 00:47:05 -0300

    some options added. wp_footer hook improved

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 # SC Instafeed
 A Instagram feed plugin for WordPress, based on InstafeedJS
 
+For now, the plugin outputs zero CSS! Soon, some CSS will be available.
+But you can add custom css into the administrator panel
+
 ## License
 [![WTFPL](wtfpl-badge.png "WTFPL")](https://github.com/zergiocosta/SC-Instafeed/blob/master/LICENSE)
--- functions/options/general.php
+++ functions/options/general.php
@@ -19,7 +19,25 @@ function sc_instafeed_settings_init(  ) {
             'sc_instafeed_sc_instafeed_section' 
         );
 
-        // clientID
+        // resolution
+        add_settings_field( 
+            'resolution', 
+            __( 'Image Resolution', 'scinstafeed' ), 
+            'sc_instafeed_resolution_render', 
+            'sc_instafeed', 
+            'sc_instafeed_sc_instafeed_section' 
+        );
+
+        // get
+        add_settings_field( 
+            'get', 
+            __( 'Content type to get', 'scinstafeed' ), 
+            'sc_instafeed_get_render', 
+            'sc_instafeed', 
+            'sc_instafeed_sc_instafeed_section' 
+        );
+
+        // tag
         add_settings_field( 
             'tag', 
             __( 'Tag to filter', 'scinstafeed' ), 
@@ -44,10 +62,32 @@ function sc_instafeed_settings_init(  ) {
 function sc_instafeed_clientid_render(  ) { 
 
     $options = get_option( 'sc_instafeed_settings' ); ?>
-    <input type="text" name="sc_instafeed_settings[clientid]" value="<?php echo $options['clientid']; ?>" placeholder="f0as8gfas98d7gas98f7gas0d87as" required>
-    
+    <input type="text" name="sc_instafeed_settings[clientid]" value="<?php echo $options['clientid']; ?>" placeholder="f0as8gfas98d7gas98f7gas0d87as" required> <?php
 
-<?php
+}
+
+// resolution
+function sc_instafeed_resolution_render(  ) { 
+
+    $options = get_option( 'sc_instafeed_settings' ); ?>
+    <select name='sc_instafeed_settings[resolution]'>
+        <option value='thumbnail' <?php selected( $options['resolution'], 'thumbnail' ); ?>>Thumbnail (150x150)</option>
+        <option value='low_resolution' <?php selected( $options['resolution'], 'low_resolution' ); ?>>Low Resolution (306x306)</option>
+        <option value='standard_resolution' <?php selected( $options['resolution'], 'standard_resolution' ); ?>>Standard (612x612)</option>
+    </select> <?php
+
+}
+
+// content type to get from instagram
+function sc_instafeed_get_render(  ) { 
+
+    $options = get_option( 'sc_instafeed_settings' ); ?>
+    <select name='sc_instafeed_settings[get]'>
+        <option value='popular' <?php selected( $options['get'], 'popular' ); ?>>Popular</option>
+        <option value='tagged' <?php selected( $options['get'], 'tagged' ); ?>>Tagged</option>
+        <?/*<option value='location' <?php selected( $options['get'], 'location' ); ?>>Location</option>
+        <option value='user' <?php selected( $options['get'], 'user' ); ?>>User</option>*/?>
+    </select> <?php
 
 }
 
@@ -55,12 +95,10 @@ function sc_instafeed_clientid_render(  ) {
 function sc_instafeed_tag_render(  ) { 
 
     $options = get_option( 'sc_instafeed_settings' ); ?>
-    <input type="text" name="sc_instafeed_settings[tag]" value="<?php echo $options['tag']; ?>">
-    
-
-<?php
+    <input type="text" name="sc_instafeed_settings[tag]" value="<?php echo $options['tag']; ?>"> <?php
 
 }
+
 
 // custom CSS
 function sc_instafeed_custom_css_render(  ) { 
@@ -68,8 +106,7 @@ function sc_instafeed_custom_css_render(  ) {
     $options = get_option( 'sc_instafeed_settings' ); ?>
     <textarea cols='40' rows='8' name='sc_instafeed_settings[custom_css]'> 
         <?php echo $options['custom_css']; ?>
-    </textarea>
-    <?php
+    </textarea> <?php
 
 }
 
@@ -85,10 +122,16 @@ function sc_instafeed_call() {
     $options = get_option( 'sc_instafeed_settings' ); ?>
     <script>
         var feed = new Instafeed({
-            get: 'tagged',
-            tagName: '<?php echo $options["tag"]; ?>',
-            clientId: '<?php echo $options["clientid"]; ?>',
-            template: '<a href="{{link}}"><img src="{{image}}" /></a>'
+            <?php if ($options["clientid"])                  { echo "clientId: '" . $options['clientid'] . "',"; } ?>
+            <?php if ($options["resolution"])                { echo "resolution: '" . $options['resolution'] . "',"; } ?>
+            <?php if ($options["get"])                       { echo "get: '" . $options['get'] . "',"; } ?>
+            <?php if ($options["tag"])                       { echo "tagName: '" . $options['tag'] . "',"; } ?>
+
+            template:   '<div class="sc_instafeed_item">\n' +
+                            '<a href="{{link}}" target="_blank">\n' +
+                                '<img src="{{image}}" />\n' +
+                            '</a>\n' + 
+                        '</div>'
         });
         feed.run();
     </script>
